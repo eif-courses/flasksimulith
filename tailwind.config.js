e7a01e795096f609ee43291b1f47e8b126f5e730
Marius <m.gzegozevskis@gmail.com>
2023-02-23 20:15:14 +0200

    update CI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html',
+   './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
